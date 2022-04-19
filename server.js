2b8294e958a24c618158014fa9354a6534ae602f
Mateus Vasconcelos <mateusvasconcelos4210@gmail.com>
2022-04-18 22:59:52 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('./models/User')
+require('dotenv').config()
+
+const app = express()
+app.use(express.json())
+
+// Credentials
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+// Mongoose Connect
+mongoose
+    .connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.og2bq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+    .then(() => {
+        app.listen(3001)
+        console.log('Connected to Mongoose...')
+    })
+    .catch((err) => console.log(err))
+
+//Login user
+app.post('/auth/login', async (req, res) => {
+    const { name, password } = req.body
+
+    const user = await User.findOne({ name })
+
+    if (!user) {
+        return res.status(422).json({ message: "Usuário não encontrado" });
+    }
+
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if (!checkPassword) {
+        return res.status(422).json({ message: "Senha incorreta" });
+    }
+
+    try {
+        const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET)
+        res.header('auth-token', token).json({ token })
+    } catch (err) {
+        console.log(err)
+    }
+})
+app.listen(3000)
